0266e069251a7d19d07367ddf7a5f20ff61345ff
DanBearLuk <danyalukashak@gmail.com>
2022-12-07 20:47:04 +0200

    b

--- server/index.js
+++ server/index.js
@@ -4,10 +4,13 @@ const cors = require('cors');
 const rateLimiter = require('express-rate-limit');
 const Config = require('./config').default;
 const jose = require('jose');
+const bcrypt = require('bcrypt');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = Config.API_URL;
 const app = express();
+
+const secret = new TextEncoder().encode('f30e7c6f861eb133858962c280d4388cf711a9a69697a72299ed9142d6c71dce');
 
 const corsOptions = {
     origin: Config.CLIENT_URL,
@@ -54,7 +57,21 @@ const initUsersDB = async () => {
 
 initUsersDB();
 
-app.use('/api/users/createAccount', createLimiter(5 * 1000, 1));
+
+async function createToken(id) {
+  const alg = 'HS256';
+  
+  const jwt = await new jose.SignJWT({ id })
+    .setProtectedHeader({ alg })
+    .setIssuedAt()
+    .setExpirationTime('720h')
+    .sign(secret);
+
+  return jwt;
+}
+
+
+app.use('/api/users/createAccount', createLimiter(5  * 1000, 1));
 app.post('/api/users/createAccount', async (req, res) => {
   if (!req.body) {
     return res.status(400).json({
@@ -68,9 +85,10 @@ app.post('/api/users/createAccount', async (req, res) => {
   const check = await usersCollection.findOne({ username });
 
   if (check) {
-    res.status(409);
-    return res.json({ success: false });
+    return res.status(409).json({ success: false });
   }
+
+  const password_hash = await bcrypt.hash(password, 10);
 
   const counter = await counterCollection.findOneAndUpdate(
     { _id: 'users' }, 
@@ -78,29 +96,82 @@ app.post('/api/users/createAccount', async (req, res) => {
     { returnDocument: 'after' }
   );
 
-  console.log(counter);
-
-  const secret = new TextEncoder().encode('f30e7c6f861eb133858962c280d4388cf711a9a69697a72299ed9142d6c71dce');
-  const alg = 'HS256'
+  const id = counter.value.seq_value;
   
-  const jwt = await new jose.SignJWT({ username, password })
-    .setProtectedHeader({ alg })
-    .setIssuedAt()
-    .setExpirationTime('720h')
-    .sign(secret)
+  const jwt = await createToken(id);
 
   const result = await usersCollection.insertOne({
-    _id: counter.value.seq_value,
+    _id: id,
     username,
-    password,
-    lastJWT: jwt
+    password: password_hash,
+    saved_list: []
   });
 
-  res.status(200);
-
-  return res.json({
+  return res.status(200).json({
     success: true,
-    user: await usersCollection.findOne({ _id: result.insertedId })
+    user: await usersCollection.findOne({ _id: result.insertedId }),
+    token: jwt
+  });
+});
+
+
+app.use('/api/users/login', createLimiter(5 * 1000, 1));
+app.post('/api/users/login', async (req, res) => {
+  let hasToken = false;
+
+  const auth = req.headers.authorization;
+  if (auth && auth.indexOf('Bearer ') !== -1) {
+    hasToken = true;
+  }
+
+  if (!hasToken && !req.body) {
+    return res.status(400).json({
+      success: false,
+      message: 'Body not found'
+    });
+  }
+
+  if (hasToken) {
+    const token = req.headers.authorization.replace('Bearer ', '');
+
+    try {
+      const { payload } = await jose.jwtVerify(token, secret);
+
+      if (payload.exp >= Date.now()) {
+        return res.status(401).json({
+          success: false,
+          message: 'Unauthorized HTTP'
+        });
+      }
+  
+      return res.status(200).json({
+        success: true,
+        user: await usersCollection.findOne({ _id: payload.id })
+      });
+    } catch {
+      return res.status(401).json({
+        success: false,
+        message: 'Unauthorized HTTP'
+      });
+    }
+  }
+
+  const { username, password } = req.body;
+  const user = await usersCollection.findOne({ username });
+
+  if (!user || !await bcrypt.compare(password, user.password)) {
+    return res.status(401).json({
+      success: false,
+      message: 'Unauthorized HTTP'
+    });
+  };
+
+  const jwt = await createToken(user._id);
+
+  return res.status(200).json({
+    success: true,
+    user,
+    token: jwt
   });
 });
 
